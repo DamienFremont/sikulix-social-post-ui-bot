6837b99abb93845ee183fcd42c51b80170900972
Damien FREMONT <damien.fremont@gmail.com>
2026-09-21 08:46:22 +0200

    fix(cara-post): 🐛 update post details for Frimousse, including new text and image filename

--- platform/cara-post.sikuli/cara-post.py
+++ platform/cara-post.sikuli/cara-post.py
@@ -1,7 +1,7 @@
 import sys.argv
 
-ARG_TXT = "20220730_220243 #pet #cat #tabbycat #france @frimoussethecat (o.o)"
-ARG_IMG_FN = "file:c:/Users/damien/SynologyDrive/workdir/project-frimousse-social/20220730_220243-compressed.jpg"
+ARG_TXT = "20240719_220112 #pet #cat #tabbycat #france @frimoussethecat (o.o)"
+ARG_IMG_FN = "file:c:/Users/damien/SynologyDrive/workdir/project-frimousse-social/20240719_220112-compressed.jpg"
 
 # parameters
 if len(sys.argv) > 1:
@@ -15,7 +15,8 @@ try:
     sleep(1)
 
     # type text
-    type(Key.TAB)
+    click("cara-post-text.png")
+    sleep(1)    
     paste(ARG_TXT)
 
     # upload picture
